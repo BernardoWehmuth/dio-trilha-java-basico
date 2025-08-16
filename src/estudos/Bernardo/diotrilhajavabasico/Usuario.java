83ce69d1dd8e603583d9d44b72d969e06d73f0cd
Bernardo Wehmuth <bernardowehmuth1109@gmail.com>
2025-08-15 21:50:50 -0300

    Update Usuario.java

--- src/estudos/Bernardo/diotrilhajavabasico/Usuario.java
+++ src/estudos/Bernardo/diotrilhajavabasico/Usuario.java
@@ -1,32 +1,43 @@
-package estudos.Bernardo.diotrilhajavabasico;
+package estudos.bernardo.diotrilhajavabasico;
+
+import java.util.Scanner;
 
 public class Usuario {
 	public static void main(String[] args) {
 		
 		SmartTV smartTv = new SmartTV();
 		
-		System.out.println("TV Ligada ? " + smartTv.ligada);
-		System.out.println("Canal Atual : " + smartTv.canal);
-		System.out.println("Volume Atual : " + smartTv.volume + "\n");
-
 		smartTv.ligar();
-		System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada + "\n");
 		
-		smartTv.desligar();
-		System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada + "\n");
+		Scanner leitor = new Scanner(System.in);
 		
-		smartTv.diminuirVolume();
-		smartTv.diminuirVolume();
-		System.out.println("Volume Atual : " + smartTv.volume + "\n");
-		
-		smartTv.aumentarVolume();
-		System.out.println("Volume Atual : " + smartTv.volume);
-		
-		smartTv.desligar();
-		smartTv.mudarCanal();
-		
-		smartTv.ligar();
-		smartTv.mudarCanal();
-		
+		while(smartTv.ligada) {
+			System.out.println("Insira a Função que deseja realizar: \n[1] - Subir Canal\n[2] - Descer Canal\n[3] - Aumentar Volume\n[4] - Diminuir Volume\n[5] - Mudar Canal\n[6] - Desligar TV\n\nEscolha: ");
+			int escolha = leitor.nextInt();
+			
+				switch(escolha) {
+					case 1:
+						smartTv.subirCanal();
+						break;
+					case 2:
+						smartTv.descerCanal();
+						break;
+					case 3:
+						smartTv.aumentarVolume();
+						break;
+					case 4:
+						smartTv.diminuirVolume();
+						break;
+					case 5:
+						smartTv.mudarCanal();
+						break;
+					case 6:
+						smartTv.desligar();
+						break;
+					default:
+						System.out.println("Insira uma opção válida!!!");
+				}
+		}
+		System.exit(0);
 	}
 }
